cf807adb2517dd38bd4215762e62830887a74785
Pedro Gonçalves <pedrofgs91@gmail.com>
2023-03-31 18:06:23 +0200

    Add a static movie list to the homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,17 @@
 <template>
-  <main>
-    <h1 class="text-4xl font-medium">Home page</h1>
+  <main class="overflow-y-auto px-8 py-4">
+    <ul class="grid grid-cols-[repeat(auto-fill,minmax(min(150px,100%),1fr))] gap-x-6 gap-y-4 p-4">
+      <li v-for="movie in movieList" :key="movie.id">
+        <MovieItem :movie="movie" />
+      </li>
+    </ul>
   </main>
 </template>
+
+<script lang="ts" setup>
+import data from "../../movie-list-example.json";
+import MovieItem from "../components/MovieItem.vue";
+import { Movie } from "../interfaces/movie";
+
+const movieList: Movie[] = data.results;
+</script>
