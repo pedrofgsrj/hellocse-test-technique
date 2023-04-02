8fd79e582ef50861cdc8a133f42639a87aa47b5f
Pedro Gonçalves <pedrofgs91@gmail.com>
2023-04-02 19:22:19 +0200

    Add scrollbar styles

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,6 @@ import router from "./router";
 import VueAwesomePaginate from "vue-awesome-paginate";
 import "vue-awesome-paginate/dist/style.css";
 import "./styles/pagination.css";
+import "./styles/scrollbars.css";
 
 createApp(App).use(router).use(VueAwesomePaginate).mount("#app");
