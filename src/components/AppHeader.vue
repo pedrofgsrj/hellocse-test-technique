313e60dfab0f5937811b81ed7eb419c503a346c4
Pedro Gonçalves <pedrofgs91@gmail.com>
2023-04-03 21:55:12 +0200

    Add logo

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,3 +1,9 @@
 <template>
-  <header class="z-10 bg-slate-800 p-4 text-white shadow shadow-slate-950/20">Logo</header>
+  <header
+    class="z-10 bg-slate-800 bg-gradient-to-b from-slate-900 to-slate-800 px-12 py-4 text-white shadow shadow-slate-950/20"
+  >
+    <router-link :to="{ name: 'Home' }">
+      <img src="../assets/logo.svg" class="h-8" alt="Logo" />
+    </router-link>
+  </header>
 </template>
